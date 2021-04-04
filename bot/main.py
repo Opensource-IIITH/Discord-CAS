ea591ec414ce286028124a6d090ae8dc3f5b57bb
shreyas <shpradhan12@gmail.com>
2021-04-04 20:44:09 +0530

    Added role adding feature

--- bot/main.py
+++ bot/main.py
@@ -7,31 +7,50 @@ from discord.ext import commands
 
 from pymongo import MongoClient
 
-mongo_client = MongoClient("mongodb+srv://dev:hidenseek@cluster0.ulzen.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = mongo_client.get_database("discord-cas")
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 
-bot = commands.Bot(command_prefix=':')
+mongo_client = MongoClient(f"mongodb+srv://dev:{MONGO_PASSWORD}@cluster0.ulzen.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = mongo_client.get_database("discord-cas")
 
-def is_verified(user_id):
-    users = list(db.users.find({"user_id": user_id}))
+bot = commands.Bot(command_prefix=';')
+
+VERIFIED_ROLE_NAME = "cas-verified"
+
+
+async def is_verified(user_id):
+    users = list(db.users.find({"user_id": str(user_id)}))
+    print(users)
     return True if len(users) else False
-    
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+
+async def assign_role(ctx, user):
+    roles = ctx.guild.roles
+    role_names = [role.name for role in roles]
+
+    if VERIFIED_ROLE_NAME not in role_names:
+        await ctx.guild.create_role(name=VERIFIED_ROLE_NAME)
+
+    required_role = [role for role in roles if role.name == VERIFIED_ROLE_NAME]
+
+    await user.add_roles(required_role[0])
 
 
 @bot.command(name="verify")
 async def verify_user(ctx):
     user_id = ctx.message.author.id
-    if is_verified(user_id):
+    verification = await is_verified(user_id)
+
+    if verification:
+        await assign_role(ctx, ctx.message.author)
         await ctx.send(f"Yayy verified")
     else:
-        await ctx.send(f"Ew not verified {user_id}")
+        await ctx.send(f"Ew not verified.")
 
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
 
 bot.run(TOKEN)
