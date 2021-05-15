eb282c84c1d0abc082cd6739564fd15677daa80f
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-05-15 18:28:51 +0530

    Added stuff and ran black Gonna host this on Azure soon

--- bot/main.py
+++ bot/main.py
@@ -8,24 +8,27 @@ from discord.ext import commands
 from pymongo import MongoClient
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
-MONGO_USER = os.getenv("MONGO_USER")
-MONGO_DATABASE= os.getenv("MONGO_DATABASE")
+TOKEN = os.getenv("DISCORD_TOKEN")
+# MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+# MONGO_USER = os.getenv("MONGO_USER")
+MONGO_DATABASE = os.getenv("MONGO_DATABASE")
+MONGO_PORT = os.getenv("MONGO_PORT")
 
-mongo_client = MongoClient(f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@cluster0.lkxfy.mongodb.net/{MONGO_DATABASE}?retryWrites=true&w=majority")
+mongo_client = MongoClient(
+    f"mongodb://127.0.0.1:{MONGO_PORT}/{MONGO_DATABASE}?retryWrites=true&w=majority"
+)
 db = mongo_client.get_database(MONGO_DATABASE)
 
-bot = commands.Bot(command_prefix=';')
+bot = commands.Bot(command_prefix=";")
 
 is_configured = False
 configured_role = None
-VERIFIED_ROLE_NAME = "cas-verified"
+VERIFIED_ROLE_NAME = "Messaging"
 
 
 async def is_verified(user_id):
     users = list(db.users.find({"discordId": str(user_id)}))
-    return True if len(users) else False
+    return True if users else False  # empty lists are false
 
 
 async def send_link(ctx):
@@ -40,6 +43,8 @@ async def assign_role(ctx, user):
 
         if VERIFIED_ROLE_NAME not in role_names:
             await ctx.guild.create_role(name=VERIFIED_ROLE_NAME)
+            # in this case, the roles list will not update in which case the following
+            # list comprehension will fail
 
         required_role = [role for role in roles if role.name == VERIFIED_ROLE_NAME][0]
 
@@ -47,7 +52,7 @@ async def assign_role(ctx, user):
         required_role = configured_role
 
     await user.add_roles(required_role)
-    await ctx.send(f"<@{user.id}> has been CAS-verified.")
+    await ctx.send(f"<@{user.id}> has been CAS-verified!")
 
 
 @bot.command(name="verify")
@@ -57,29 +62,42 @@ async def verify_user(ctx):
 
     if verification:
         await assign_role(ctx, ctx.message.author)
-        await ctx.send(f"Yayy verified")
     else:
         await send_link(ctx)
 
 
+# Practically, this function is not useful
+# because the configured role is reset in case you restart the bot
+# so it would be better to just change the value in some configuration file instead
 @commands.has_permissions(administrator=True)
 @bot.command(name="configure")
 async def configure_verification_role(ctx, role: discord.Role):
+    """
+    This function configures the verification role for the server.
+
+    :param ctx:
+    :param role:
+    :return:
+    """
     global configured_role, is_configured
     configured_role = role
     is_configured = True
-    await ctx.send(f"Verified role set to {role.name}")    
+    await ctx.send(f"Verified role set to {role.name}")
+
 
 @configure_verification_role.error
 async def configure_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send("You must be an admin to configure roles")
-    elif isinstance(error, commands.MissingRequiredArgument) or isinstance(error, commands.RoleNotFound):
+    elif isinstance(error, commands.MissingRequiredArgument) or isinstance(
+            error, commands.RoleNotFound
+    ):
         await ctx.send("Please specify a role.")
 
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f"{bot.user.name} has connected to Discord!")
+
 
 bot.run(TOKEN)
